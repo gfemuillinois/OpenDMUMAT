27e4d1f75fe0a6d2beabe7d38fa4e4ce28b8a918
Bryce Mazurowski <brycepm2@gmail.com>
2023-12-13 16:48:20 -0600

    Added test case for OxOx material

--- umat_opendm.cpp
+++ umat_opendm.cpp
@@ -14,8 +14,14 @@ using std::endl;
 
 // declarations 
 /** @brief Runs simple matPt test on strain controlled load
+ *  for GE SiCSiC material provided by Craig Przybyla AFRL
  */
-int runMatPtTest(const int modelVal);
+int runGEMatPtTest(const int modelVal);
+
+/** @brief Runs simple matPt test on strain controlled load
+ *  for Boeing OxOx material
+ */
+int runOxOxMatPtTest(const int modelVal);
 
 /** @brief Runs TaylorCheck (tm Nathan Shauer) to test
  * material tangent matrix
@@ -83,7 +89,7 @@ int main(int argc, char* argv[]) {
   }
   std::cout << "Using model: " << modelVal << std::endl;
   if (true) {
-    const int test = runMatPtTest(modelVal);
+    const int test = runOxOxMatPtTest(modelVal);
     return test;
   } else {
     const double convVal = numTangCalc(modelVal);
@@ -93,7 +99,7 @@ int main(int argc, char* argv[]) {
 /********************************************************************/
 /********************************************************************/
 
-int runMatPtTest(const int modelVal) {
+int runOxOxMatPtTest(const int modelVal) {
   // elastic material definitions
   double E11, E22, E33, nu12, nu23, nu13, G12, G23, G13;
   // damage model parameters
@@ -110,72 +116,263 @@ int runMatPtTest(const int modelVal) {
   double loadApplied;
   double maxStrain;
   int nInc = 100;
-  const bool pythonTest = true;
-  if ( !pythonTest ) {
-    // set elasticity
-    E11 = 12.416e6; E22 = E11; E33 = 7.513e6;
-    nu12 = 0.1106; nu23 = 0.1707; nu13 = nu23;
-    G12 = 3.332e6; G23 = 0.670e6; G13 = G23;
-    // damage model parameters
-    hs1_11 = 2.5; hs1_12 = hs1_11; hs1_13 = hs1_11;
-    hs2_22 = 2.5; hs2_12 = hs2_22; hs2_23 = hs2_22;
-    hs4_11 = 2.5; hs4_12 = hs4_11; hs4_13 = hs4_11; hs4_16 = hs4_11;
-    hs5_11 = 2.5; hs5_12 = hs5_11; hs5_13 = hs5_11; hs5_16 = hs4_11;
-    b1 = 0.2; b2 = b1; b6 = b1;
-    y01 = 0.005; y02 = y01; y04 = y01; y05 = y01;
-    yc1 = 500; yc2 = yc1; yc4 = yc1; yc5 = yc1;
-    pe1 = 1.5; pe2 = pe1; pe4 = pe1; pe5 = pe1;
-    dc1 = 2.0; dc2 = dc1; dc4 = dc1; dc5 = dc1;
-    // load
-    maxStrain = 0.003;
-  } else {
-    // Elastic Properties
-    E11 = 335000;       //Young Modulus [MPa]
-    E22 = 160000;       //Young Modulus [MPa]
-    E33 = 335000;       //Young Modulus [MPa]
-    nu12 = 0.17;         //Poisson ratio
-    nu23 = 0.00;         //Poisson ratio
-    nu13 = 0.00;         //Poisson ratio
-    G12 = 95360;        //Shear modulus [MPa]
-    G23 = 95360;        //Shear modulus [MPa]
-    G13 = 95360;        //Shear modulus [MPa]
-    
-    // Material State Variables
-    hs1_11 = 1.2;        //Damage Effect
-    hs1_12 = 1.0;        //Damage Effect
-    hs1_13 = 1.0;        //Damage Effect
-    hs2_22 = 20.0;       //Damage Effect
-    hs2_12 = 1.0;        //Damage Effect
-    hs2_23 = 1.0;        //Damage Effect
-    hs4_11 = 2.0;        //Damage Effect
-    hs4_12 = 0.5;        //Damage Effect
-    hs4_13 = 0.5;        //Damage Effect
-    hs4_16 = 1.0;        //Damage Effect
-    hs5_11 = 2.0;        //Damage Effect
-    hs5_12 = 0.5;        //Damage Effect
-    hs5_13 = 0.5;        //Damage Effect
-    hs5_16 = 1.0;        //Damage Effect
-    b1 = 0.3;           //Traction / Shear Coupling 1
-    b2 = 0.3;           //Traction / Shear Coupling 2
-    b6 = 0.3;           //Traction / Shear Coupling 2
-    y01 = 0.3;         //Damage Thresholds 1
-    y02 = 0.04;         //Damage Thresholds 2
-    y04 = 0.04;         //Damage Thresholds 1
-    y05 = 0.04;         //Damage Thresholds 2
-    yc1 = 16;           //Damage Evolution Celerity 1
-    yc2 = 0.22;         //Damage Evolution Celerity 2
-    yc4 = 10;           //Damage Evolution Celerity 1
-    yc5 = 10;          //Damage Evolution Celerity 2
-    pe1 = 1.15;         //Damage Evolution Exponents 1
-    pe2 = 2.4;          //Damage Evolution Exponents 2
-    pe4 = 1.1;        //Damage Evolution Exponents 1
-    pe5 = 1.1;          //Damage Evolution Exponents 2
-    dc1 = 5.5;          //Damage Saturations 1
-    dc2 = 1.0;         //Damage Saturations 2
-    dc4 = 8;          //Damage Saturations 1
-    dc5 = 8;         //Damage Saturations 2
-    maxStrain = 0.01;
+  // set elasticity
+  E11 = 12.416e6;
+  E22 = E11;
+  E33 = 7.513e6;
+  nu12 = 0.1106;
+  nu23 = 0.1707;
+  nu13 = nu23;
+  G12 = 3.332e6;
+  G23 = 0.670e6;
+  G13 = G23;
+  // damage model parameters
+  hs1_11 = 1.71;
+  hs1_12 = 2.5;
+  hs1_13 = 2.5;
+  hs2_22 = 1.71;
+  hs2_12 = 2.5;
+  hs2_23 = 2.5;
+  hs4_11 = 2.67;
+  hs4_12 = 3.01;
+  hs4_13 = 2.5;
+  hs4_16 = 2.5;
+  hs5_11 = 2.67;
+  hs5_12 = 3.01;
+  hs5_13 = 2.5;
+  hs5_16 = 2.5;
+  b1 = 0.2;
+  b2 = b1;
+  b6 = b1;
+  y01 = 0.005;
+  y02 = y01;
+  y04 = y01;
+  y05 = y01;
+  yc1 = 500;
+  yc2 = yc1;
+  yc4 = yc1;
+  yc5 = yc1;
+  pe1 = 1.5;
+  pe2 = pe1;
+  pe4 = 1.38;
+  pe5 = 1.38;
+  dc1 = 1.39;
+  dc2 = 1.39;
+  dc4 = 5.16;
+  dc5 = 5.16;
+  // load
+  maxStrain = 0.003;
+
+  // need to set a bunch of stuff
+  double strain[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+  double dstrain[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+  double stress[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+  double ddsdde[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+  char cmname[80] = "OpenDM";
+  int nprops, nstatv;
+  // Set size to be largest possible
+  double props[42];
+  // elastic props
+  props[0] = E11; props[1] = E22; props[2] = E33;
+  props[3] = nu12; props[4] = nu23; props[5] = nu13;
+  props[6] = G12; props[7] = G23; props[8] = G13;
+  if (modelVal == 2) {
+    cmname[6] = '2';
+    nprops = 21; // Number of properties
+    nstatv = 13; // number of statVars
+    double props2Param[] = {hs1_11, hs2_22, b1,  b2, y01, y02,
+                            yc1, yc2, pe1, pe2, dc1, dc2};
+    for (int iProp = 0; iProp < 12; iProp++) {
+      props[9+iProp] = props2Param[iProp];
+    }
+  } else if (modelVal == 4) {
+    cmname[6] = '4';
+    nprops = 42; // Number of properties
+    nstatv = 17; // Number stateVars
+    double props4Param[] = {hs1_11, hs2_12, hs1_13,
+                            hs2_22, hs2_12, hs2_23,
+                            hs4_11, hs4_12, hs4_13, hs4_16,
+                            hs5_11, hs5_12, hs5_13, hs5_16,
+                            b1, b2, b6,
+                            y01, y02, y04, y05,
+                            yc1, yc2, yc4, yc5,
+                            pe1, pe2, pe4, pe5,
+                            dc1, dc2, dc4, dc5};
+    for (int iProp = 0; iProp < 33; iProp++) {
+      props[9+iProp] = props4Param[iProp];
+    }
   }
+  double sse, spd, scd, rpl;
+  // Current deltaT for which solution is seeking
+  double dTime = 0.001;
+  double time[] = {0.0, 0.001};
+
+  int ndi = 3;            // 3 direct stress/strain components
+  int nshr = 3;           // 3 shear stress/strain components
+  int ntens = ndi + nshr; // 6 total componenets. ndi+nshr
+  int noel = 1;
+  int npt = 0; // Integration point number
+  double ddsddt = 0.;
+  double drplde = 0.;
+  double drpldt = 0.;
+  double temp = 0.;
+  double dTemp = 0.;
+  double predef = 0.;
+  double dpred = 0.;
+  double coords = 0.;
+  double drot = 0.;
+  double pnewdt = 0.;
+  double dfgrd0 = 0.;
+  double dfgrd1 = 0.;
+  double celent;
+  int layer, kspt, kstep, kinc;
+  layer = kspt = kstep = kinc = 0;
+
+  // setup a loop to call umat each step
+  double stepSize = maxStrain / (nInc);
+  std::ofstream myFile;
+  std::string modelNum = std::to_string(modelVal);
+  for (int runInt = 0; runInt < 5; ++runInt) {
+    cout << "Running " << runInt << endl;
+    // file for each run
+    std::string runStr = std::to_string(runInt);
+    std::string fileName = "umat_OxOxTestOut_"+modelNum+"_Run"+runStr+".csv";
+    std::string sep = ", ";
+    myFile.open(fileName);
+    myFile << "step, "
+           << "strain11, strain22, strain12, "
+           << "stress11, stress22, stress12, "
+           << "d1, d2, d4, d5" << std::endl;
+    // zero out SVs for each new load
+    double statev[17] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    for (int iPull = 0; iPull <= nInc; iPull++) {
+      // Increment dstrain
+      switch(runInt) {
+      case 0:
+        // Aligned Tension
+        dstrain[0] = iPull * stepSize;
+        dstrain[1] = 0.0;
+        dstrain[3] = 0.0;
+        break;
+      case 1:
+        // 90 degree tension
+        dstrain[0] = 0.0;
+        dstrain[1] = iPull*stepSize;
+        dstrain[3] = 0.0;
+        break;
+      case 2:
+        // +/- 45 tension
+        dstrain[0] = iPull*0.5*stepSize;
+        dstrain[1] = iPull*0.5*stepSize;
+        dstrain[3] = iPull*stepSize;
+        break;
+      case 3:
+        // +/- 45 tension
+        dstrain[0] = iPull*0.5*stepSize;
+        dstrain[1] = iPull*0.5*stepSize;
+        dstrain[3] = -1.0*iPull*stepSize;
+        break;
+      case 4:
+        // Aligned Compression 
+        dstrain[0] = -iPull * stepSize;
+        dstrain[1] = 0.0;
+        dstrain[3] = 0.0;
+        break;
+      }
+      // run model
+      clock_t start = clock();
+      (umat)(stress, statev, ddsdde, &sse, &spd, &scd, &rpl, &ddsddt, &drplde,
+             &drpldt, strain, dstrain, time, &dTime, &temp, &dTemp, &predef,
+             &dpred, cmname, &ndi, &nshr, &ntens, &nstatv, props, &nprops,
+             &coords, &drot, &pnewdt, &celent, &dfgrd0, &dfgrd1, &noel, &npt,
+             &layer, &kspt, &kstep, &kinc);
+
+      clock_t end = clock();
+      std::cout << "step = " << iPull << " Time = " << (end - start) << std::endl;
+      std::cout << "Strain: " << std::endl;
+      printVec(dstrain);
+      std::cout << "Stress: " << std::endl;
+      printVec(stress);
+      myFile << iPull << sep
+             << dstrain[0] << sep << dstrain[1] << sep << dstrain[3] << sep
+             << stress[0] << sep << stress[1] << sep << stress[3] << sep 
+             << statev[13] << sep << statev[14] << sep << statev[15] << sep << statev[16] << sep
+             << std::endl;
+    }
+    myFile.close();
+  }
+  return 0;
+ }
+/********************************************************************/
+/********************************************************************/
+
+int runGEMatPtTest(const int modelVal) {
+  // elastic material definitions
+  double E11, E22, E33, nu12, nu23, nu13, G12, G23, G13;
+  // damage model parameters
+  double hs1_11, hs1_12, hs1_13,
+    hs2_22, hs2_12, hs2_23,
+    hs4_11, hs4_12, hs4_13, hs4_16,
+    hs5_11, hs5_12, hs5_13, hs5_16,
+    b1, b2, b6,
+    y01, y02, y04, y05,
+    yc1, yc2, yc4, yc5,
+    pe1, pe2, pe4, pe5,
+    dc1, dc2, dc4, dc5;
+  // applied load
+  double loadApplied;
+  double maxStrain;
+  int nInc = 100;
+  // Elastic Properties
+  E11 = 335000; // Young Modulus [MPa]
+  E22 = 160000; // Young Modulus [MPa]
+  E33 = 335000; // Young Modulus [MPa]
+  nu12 = 0.17;  // Poisson ratio
+  nu23 = 0.00;  // Poisson ratio
+  nu13 = 0.00;  // Poisson ratio
+  G12 = 95360;  // Shear modulus [MPa]
+  G23 = 95360;  // Shear modulus [MPa]
+  G13 = 95360;  // Shear modulus [MPa]
+
+  // Material State Variables
+  hs1_11 = 1.2;  // Damage Effect
+  hs1_12 = 1.0;  // Damage Effect
+  hs1_13 = 1.0;  // Damage Effect
+  hs2_22 = 20.0; // Damage Effect
+  hs2_12 = 1.0;  // Damage Effect
+  hs2_23 = 1.0;  // Damage Effect
+  hs4_11 = 2.0;  // Damage Effect
+  hs4_12 = 0.5;  // Damage Effect
+  hs4_13 = 0.5;  // Damage Effect
+  hs4_16 = 1.0;  // Damage Effect
+  hs5_11 = 2.0;  // Damage Effect
+  hs5_12 = 0.5;  // Damage Effect
+  hs5_13 = 0.5;  // Damage Effect
+  hs5_16 = 1.0;  // Damage Effect
+  b1 = 0.3;      // Traction / Shear Coupling 1
+  b2 = 0.3;      // Traction / Shear Coupling 2
+  b6 = 0.3;      // Traction / Shear Coupling 2
+  y01 = 0.3;     // Damage Thresholds 1
+  y02 = 0.04;    // Damage Thresholds 2
+  y04 = 0.04;    // Damage Thresholds 1
+  y05 = 0.04;    // Damage Thresholds 2
+  yc1 = 16;      // Damage Evolution Celerity 1
+  yc2 = 0.22;    // Damage Evolution Celerity 2
+  yc4 = 10;      // Damage Evolution Celerity 1
+  yc5 = 10;      // Damage Evolution Celerity 2
+  pe1 = 1.15;    // Damage Evolution Exponents 1
+  pe2 = 2.4;     // Damage Evolution Exponents 2
+  pe4 = 1.1;     // Damage Evolution Exponents 1
+  pe5 = 1.1;     // Damage Evolution Exponents 2
+  dc1 = 5.5;     // Damage Saturations 1
+  dc2 = 1.0;     // Damage Saturations 2
+  dc4 = 8;       // Damage Saturations 1
+  dc5 = 8;       // Damage Saturations 2
+  maxStrain = 0.01;
 
   // need to set a bunch of stuff
   double strain[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
